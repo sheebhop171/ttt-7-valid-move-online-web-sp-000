3422423af9d025a123f71c17e1e16da28a771e48
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-09 21:19:58 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,6 @@
-def input_to_index(user_input)
-  return user_input = user_input.to_i - 1
-end
-
 # code your #valid_move? method here
 def valid_move?(board,index)
   #position is on board and position is not taken
-  if input_to_index.between(0,8)
   #return true
   #position is not on board or is taken
   #return false
