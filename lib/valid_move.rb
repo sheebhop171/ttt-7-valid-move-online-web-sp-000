dce31f57b3517898e7577143462de479c190be7e
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-09 21:13:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -20,11 +20,3 @@ def position_taken?(board,index)
     false
   end
 end
-
-#def some_new_fabulous_method
-    #if position_taken?
-        #execute something
-    #else
-        #execute something else
-    #end
-#end
