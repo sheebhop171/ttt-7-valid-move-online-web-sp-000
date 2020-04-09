8c94ff114d14804041281345b41563f28a70a440
Shelby Hopkins <shelby.hopkins171@gmail.com>
2020-04-09 21:26:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,18 +7,6 @@ def valid_move?(board,index)
   end
 end
 
-#valid position on empty board
-#return true
-
-#valid position on non-empty board
-#return true
-
-#if occupied position
-#return nil
-
-#if position is not on board
-#return nil
-
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,index)
   if board[index] == "X" || board[index] == "O"
